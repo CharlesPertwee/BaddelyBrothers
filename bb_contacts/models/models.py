42d7bbd57a8be4ca5de10fc303a8c86edbbb1abf
Charles Pertwee <charlespertwee@baddelybrothers.com>
2019-03-08 16:54:56 +0000

    Search Popup fix

--- bb_contacts/models/models.py
+++ bb_contacts/models/models.py
@@ -27,9 +27,19 @@ class ContactLink(models.Model):
     
     contact = fields.Many2one('res.partner',string='Contact',domain="[('is_company','=',False)]",required=True)
     
-    is_company = fields.Boolean('Company')
+    contactLink_id = fields.Many2one('res.partner')
+    companyLink_id = fields.Many2one('res.partner')
     
-    
+    @api.model
+    def create(self,values):
+        values['contactLink_id'] = values['contact']
+        values['companyLink_id'] = values['company']
+        record = super(ContactLink,self).create(values)
+        #data = {'contactLinks': [(4,record.id)]}
+        #record.company.write(data)
+        #record.contact.write(data)
+        return record
+        
 class Partner(models.Model):
     _inherit = 'res.partner'
     _name = 'res.partner'
@@ -45,23 +55,7 @@ class Partner(models.Model):
     contactExtention = fields.Char('Contact Extention')
     mainContact = fields.Boolean('Main Contact')
     
-    contactLinks = fields.Many2many('bb_contacts.contacts_link',string='Contacts')
+    companyLinks = fields.One2many('bb_contacts.contacts_link','companyLink_id',string='Comapanies')
+    contactLinks = fields.One2many('bb_contacts.contacts_link','contactLink_id',string='Contacts')
     
-# class CompanyLink(models.Model):
-#     _name = 'bb_contacts.company_link'
-    
-#     status = fields.Selection([('current','Current'),('past','Past')],required='True',string="State",default="current")
-#     company = fields.Many2one('res.partner',string="Company",domain="[('is_company','=',False)]",required=True)
-#     jobTitle = fields.Many2one('bb_contacts.job',string="Job Title")
-#     jobRole = fields.Selection([('owner','Bussiness Owner'),('department_manager','Departmental Manager'),('finance_excetive','Finance Executive'),('sale_executive','Sales Executive'),('purchase','Purchase'),('account_executive','Account Executive'),('production','Production'),('complaint_dept','Complaints Dept')],string="Job Role")
-#     relationship = fields.Text(string="Relationship")        
-#     address = fields.Char(string="Address")
-#     phone = fields.Integer(string="Phone")
-#     mobile = field.Integer(string="Mobile Phone")
-#     extension = fields.Text(string="Extension")
-#     email = fields.Char(string='E-mail')
-#     fax = fields.Integer(string='Fax-No')
-#     #fields for map_view
-#     joiningDate = fields.Date(string="Joining Date")
-#     leavingDate = fields.Date(string="Leaving Date")
-#     
+  
