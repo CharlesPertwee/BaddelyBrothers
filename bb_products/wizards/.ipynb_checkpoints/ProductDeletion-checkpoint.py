33ebda59c2ffdab27f75dce7b9e5aff23dd9574b
divy9t <divy9t12@icloud.com>
2019-08-28 07:05:42 +0000

    Handling Extras Part

--- bb_products/wizards/.ipynb_checkpoints/ProductDeletion-checkpoint.py
+++ bb_products/wizards/.ipynb_checkpoints/ProductDeletion-checkpoint.py
@@ -6,39 +6,14 @@ from odoo.exceptions import MissingError, UserError, ValidationError, AccessErro
 
 class ProductDeletion(models.TransientModel):
     _name = "product.delete_by_date"
-    
-    deleteBy = fields.Selection([('Date','Date Before'),('Month','No of Months Before')], string="Delete By", default = "Date")
     deletionDate = fields.Date(string="Delete Before Date",required=True)
-    deletionMonth = fields.Integer(string="Delete Before Month")
     
     def DeleteProducts(self):
-        products = self.env['product.template'].sudo().search([('active','=',True)])
-        noUsedProducts = True
-        if self.deleteBy == "Date":
-            date = datetime.datetime(self.deletionDate.year, self.deletionDate.month, self.deletionDate.day)
-            if date > datetime.datetime.now():
-                raise ValidationError("You can't enter the future date.")
-            
-            for product in products:
-                if product.lastUsedEstimateDate and (product.lastUsedEstimateDate < self.deletionDate):
-                    noUsedProducts = False
-                    product.write({'active':False})
-            
-            if noUsedProducts:
-                raise ValidationError("No products found.")
-                    
-        if self.deleteBy == "Month":
-            if self.deletionMonth <= 0:
-                raise ValidationError("Invalid month entry")
-            
-            deletionDate = datetime.datetime.now() - datetime.timedelta(days= self.deletionMonth * 30)
-            
-            for product in products:
-                if product.lastUsedEstimateDate:
-                    date = datetime.datetime(product.lastUsedEstimateDate.year, product.lastUsedEstimateDate.month, product.lastUsedEstimateDate.day)
-                    if product.lastUsedEstimateDate and (date < deletionDate):
-                        noUsedProducts = False
-                        product.write({'active':False})
-                    
-            if noUsedProducts:
-                raise ValidationError("No products found.")
+        if self.deletionDate > datetime.date.today():
+            raise ValidationError("You can't enter the future date.")
+        
+        products = self.env['product.template'].sudo().search([('active','=',True),('lastUsedEstimateDate','!=',False),('lastUsedEstimateDate','<',self.deletionDate)])
+        products.write({'active':False})
+        
+        if len(products) == 0:
+            raise ValidationError("%s Products Archived."%(len(products)))
